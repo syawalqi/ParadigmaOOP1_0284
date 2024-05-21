8217b2cbd11c92d1e068b4b80913d24ea219fd2b
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-21 15:59:16 +0700

    membuat void ibu cetak anak

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -20,3 +20,12 @@ class ibu {
 void ibu::tambahAnak(anak* pAnak) {
     daftar_anak.push_back(pAnak);
 }
+
+void ibu::cetakAnak(){
+    cout << "Daftar Anak dari Ibu  \"" << this->nama << "\" : \n";
+    for (auto& a : daftar_anak) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+#endif
