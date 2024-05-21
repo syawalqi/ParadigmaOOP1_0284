bf3274cef87037f80cb269465dbbee8629f70f4b
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-21 14:53:04 +0700

    membuat member definisi

--- memberintlist.cpp
+++ memberintlist.cpp
@@ -8,4 +8,8 @@ class mahasiswa {
     const int id;
     string nama;
     float nilai;
+
+    public:
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {// memeber definisi
+    }
 }
