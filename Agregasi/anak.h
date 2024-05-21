a1c0c2736c6387cdf58d6828456fe72241756d33
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-21 15:46:49 +0700

    membuat destructor dan mengakhiri file

--- Agregasi/anak.h
+++ Agregasi/anak.h
@@ -7,4 +7,9 @@ class anak {
     anak(string pNama) :nama(pNama) {
         cout << "Anak\""<< nama << "\" ada\n";
     }
-}
+
+    ~anak() {
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
+#endif
