a8bb70eeef35de580c9e04251984665233d6f33a
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-22 06:56:17 +0700

    membuat inheritance

---
+++ inheritance.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang
+{
+public:
+    string nama;
+
+    orang(string pNama) : nama(pNama)
+    {
+        cout << "Orang dibuat\n";
+    }
+
+    ~orang()
+    {
+        cout << "Orang dihapus\n";
+    }
+
+    int jumlah(int a, int b)
+    {
+        return a + b;
+    }
+};
+
+class pelajar : public orang
+{
+public:
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah)
+    {
+        cout << "Pelajar dibuat\n";
+    }
+
+    ~pelajar()
+    {
+        cout << "Pelajar dihapus\n";
+    }
+
+    string perkenalan()
+    {
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n";
+    }
+};
+
+int main()
+{
+    pelajar siswa1("Andi Laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+    return 0;
+}
